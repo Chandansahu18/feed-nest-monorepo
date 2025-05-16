9fe8b283b649468c822b326ec6e7ae017c0fc5de
Chandan Sahu <75119392+Chandansahu18@users.noreply.github.com>
2025-05-16 12:53:11 +0530

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+    <div className="bg-black h-screen w-screen">
+      <p className="text-white">
+        Hello World!
+        </p>
+    </div>
+    </>
+  )
+}
+
+export default App
