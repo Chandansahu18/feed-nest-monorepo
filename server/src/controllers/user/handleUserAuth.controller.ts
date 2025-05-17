ac19c8dc8e60341d535506f099021d7ab27f99dd
Chandan Sahu <75119392+Chandansahu18@users.noreply.github.com>
2025-05-17 14:58:54 +0530

    feat: userAuth controller

--- server/src/controllers/user/handleUserAuth.controller.ts
+++ server/src/controllers/user/handleUserAuth.controller.ts
@@ -1,42 +1,59 @@
-import { PrismaClient } from '../../../generated/prisma';
+import { PrismaClient, User } from '../../../generated/prisma';
 import { Request, Response } from 'express';
 
 const prisma = new PrismaClient();
 
-const handleUserAuth = async (req: Request, res: Response) => {
-  const { email, userName, name, password } = req.body;
+interface IResponseSchema {
+  success: boolean;
+  message: string;
+  data: User;
+}
 
-  const existingUser = await prisma.user.findUnique({
-    where: {
-      email:email
-    },
-  });
+const handleUserAuth = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, userName, name, password } = req.body;
 
-  if (!existingUser) {
-    const newUser = await prisma.user.create({
-      data: {
-        name,
-        userName,
-        email,
-        hashedpassword: password,
-        accessToken: '1234',
-        refreshToken: '1234',
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: email,
       },
     });
 
-     res.status(200).json({
-      success: true,
-      message: 'User has been registered successfully.',
-      data: newUser,
-    });
-    return;
-  }
-   res.status(200).json({
-        success: true,
-        message: "User already exists.",
-        data: existingUser
-    });
+    if (!existingUser) {
+      const newUser = await prisma.user.create({
+        data: {
+          name,
+          userName,
+          email,
+          hashedpassword: password,
+          accessToken: '1234',
+          refreshToken: '1234',
+        },
+      });
 
+      res.status(200).json({
+        success: true,
+        message: 'User has been registered successfully.',
+        data: newUser,
+      });
+      return;
+    }else{
+      res.status(200).json({
+        success: true,
+        message: 'User already exist.',
+        data: existingUser,
+      });
+      return;
+    }
+  } catch (error: unknown) {
+    const errorMessage = error instanceof Error ? error.message : 'Internal server error, please try again later';
+    
+    res.status(500).json({
+      success: false,
+      message: errorMessage,
+      error: error 
+    });
+}
 };
 
 export default handleUserAuth;
