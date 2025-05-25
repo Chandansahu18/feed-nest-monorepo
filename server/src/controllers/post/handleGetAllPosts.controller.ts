8fd7a25b279d6f9b2df18df234fc9fdbb77b196e
Chandan Sahu <75119392+Chandansahu18@users.noreply.github.com>
2025-05-25 19:10:00 +0530

    feat: add all posts controller

--- server/src/controllers/post/handleGetAllPosts.controller.ts
+++ server/src/controllers/post/handleGetAllPosts.controller.ts
@@ -1,6 +1,27 @@
+import { PrismaClient } from '../../../generated/prisma';
+import { Request, Response } from 'express';
+const prisma = new PrismaClient();
 
+const handleGetAllPosts = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const allPosts = await prisma.post.findMany();
+    res.status(200).json({
+      success: true,
+      data: allPosts,
+    });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error
+        ? error.message
+        : 'Internal server error, please try again later';
 
-const handleGetAllPosts = () =>{
-
-}
+    res.status(500).json({
+      success: false,
+      message: errorMessage,
+    });
+  }
+};
 export default handleGetAllPosts;
