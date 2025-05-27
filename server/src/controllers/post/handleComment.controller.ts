759c12d4037d018f8ddb44803f712c1b0919e107
Chandan Sahu <75119392+Chandansahu18@users.noreply.github.com>
2025-05-27 13:47:19 +0530

    feat: add post-comment & reply controller

--- server/src/controllers/post/handleComment.controller.ts
+++ server/src/controllers/post/handleComment.controller.ts
@@ -1,6 +1,60 @@
+import { PrismaClient } from '../../../generated/prisma';
+import { Request, Response } from 'express';
+import { IRequest } from '../../utils/types';
 
-const handleComment = () =>{
+const prisma = new PrismaClient();
 
+const handleComment = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email } = req as IRequest;
+    const { postId, content, postCommentId } = req.body;
 
-}
+    if (!content) {
+      throw new Error('Missing required content');
+    }
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!postId) {
+      const commentReply = await prisma.commentReply.create({
+        data: {
+          replierId: user?.id as string,
+          postCommentId,
+          reply: content,
+        },
+      });
+      res.status(201).json({
+        success: true,
+        message: 'Reply submitted successfully',
+        data: commentReply,
+      });
+    } else {
+      const postComment = await prisma.postComment.create({
+        data: {
+          commentorId: user?.id as string,
+          postId,
+          comment: content,
+        },
+      });
+      res.status(201).json({
+        success: true,
+        message: 'Comment submitted successfully',
+        data: postComment,
+      });
+    }
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error
+        ? error.message
+        : 'Internal server error, please try again later';
+
+    res.status(500).json({
+      success: false,
+      message: errorMessage,
+    });
+  }
+};
 export default handleComment;
