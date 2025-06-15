9f9ef839a19ac4e50ec8c8165f30ab409c421b42
Chandan Sahu <75119392+Chandansahu18@users.noreply.github.com>
2025-06-15 23:11:43 +0530

    refactor: user data controller

--- server/src/controllers/user/handleGetUser.controller.ts
+++ server/src/controllers/user/handleGetUser.controller.ts
@@ -3,40 +3,13 @@ import { Request, Response } from 'express';
 import { IRequest } from '../../utils/types';
 
 const prisma = new PrismaClient();
-const handleGetUser = async (req: Request, res: Response) => {
+const handleGetUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const { email } = req as IRequest;
     const { id } = req.query;
-    if (!email) {
-      throw new Error('Unauthorized access - please login again');
-    }
-    if (!id) {
-      // If id is not provided from query parameters then authenticated user will get its own data 
-      const user = await prisma.user.findFirst({
-        where: {
-          email,
-          isEmailVerified: true,
-        },
-        omit: {
-          hashedPassword: true,
-          refreshToken: true,
-        },
-      });
+    if (!email && id) {
+      console.log(id);
       
-      if (!user) {
-        res.status(404).json({
-          success: false,
-          message: 'User not found',
-        });
-      }
-      
-      res.status(200).json({
-        success: true,
-        message: 'User data retrieved successfully',
-        data: user,
-      });
-    }else{
-      // If id is present then data of specific user's id will be provided 
       const user = await prisma.user.findFirst({
         where: {
           id: id as string,
@@ -47,26 +20,51 @@ const handleGetUser = async (req: Request, res: Response) => {
           refreshToken: true,
         },
       });
+  
       if (!user) {
         res.status(404).json({
           success: false,
-          message: 'User not found',
+          message: 'User not found'
         });
       }
-      
+  
+      res.status(200).json({
+        success: true,
+        message: 'User data retrieved successfully',
+        data: user,
+      });      
+    }else if (!id && email) {
+      const user = await prisma.user.findFirst({
+        where: {
+          email,
+          isEmailVerified: true,
+        },
+        omit: {
+          hashedPassword: true,
+          refreshToken: true,
+        },
+      });
+  
+      if (!user) {
+        res.status(404).json({
+          success: false,
+          message: 'User not found'
+        });
+      }
+  
       res.status(200).json({
         success: true,
         message: 'User data retrieved successfully',
         data: user,
       });
-
+    }else{
+     throw new Error("Required parameters not found")
     }
   } catch (error) {
     const errorMessage =
-    error instanceof Error
-    ? error.message
-    : 'Internal server error, please try again later';
-    console.log(errorMessage);
+      error instanceof Error
+        ? error.message
+        : 'Internal server error, please try again later';
     res.status(500).json({
       success: false,
       message: errorMessage,
