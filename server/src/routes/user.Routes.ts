b1a407802d9955d55f75c44ba9c08eed27553eb4
Chandan Sahu <75119392+Chandansahu18@users.noreply.github.com>
2025-06-15 22:41:26 +0530

    revamp: add authenticated user route & refactor user controllers

--- server/src/routes/user.Routes.ts
+++ server/src/routes/user.Routes.ts
@@ -5,10 +5,12 @@ import handleSearch from "../controllers/user/handleSearch.controller";
 import handleUserDetailsUpdate from "../controllers/user/handleUserDetailsUpdate.controller";
 import handleUserAccountDelete from "../controllers/user/handleUserAccountDelete.controller";
 import handleUserPostSave from "../controllers/user/handleUserPostSave.controller";
+import handleGetAuthenticatedUser from "../controllers/user/handleGetAuthenticatedUser.controller";
 
 const router = express.Router();
 
-router.route("/v1/user").get(handleGetUser).patch(handleUserDetailsUpdate).delete(handleUserAccountDelete);
+router.route("/v1/user").get(handleGetUser).get(handleGetAuthenticatedUser).patch(handleUserDetailsUpdate).delete(handleUserAccountDelete);
+router.route("/v1/user/saved").get(handleGetUserSavedPosts).post(handleUserPostSave);
 router.route("/v1/user/saved").get(handleGetUserSavedPosts).post(handleUserPostSave);
 router.route("/v1/search").get(handleSearch);
 
