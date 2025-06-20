20c2d9c41cab3a3a7edb0133fa3ae1f9cfae6837
Chandan Sahu <75119392+Chandansahu18@users.noreply.github.com>
2025-06-20 13:56:22 +0530

    feat: add header component & routes

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,25 +1,104 @@
-import { Button } from "./ui/button"
-
+import { useNavigate } from "react-router-dom";
+import { useTheme } from "./theme-provider";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./mode-toggle";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "./ui/dropdown-menu";
+import { Menu } from "lucide-react";
 const Header = () => {
+  const { theme } = useTheme();
+  const navigate = useNavigate();
+  const handleSignIn = () => {
+    navigate("/auth");
+  };
+  const handleFeed = () => {
+    navigate("/feeds");
+  };
+  const handleAbout = () => {
+    navigate("/about");
+  };
+
   return (
     <div className="h-16 px-4">
       {/* main header content */}
-     <div className="h-full flex justify-between items-center">
-     {/* left content */}
-     <div className="h-10 w-32 flex justify-start items-center">
-      <button className="size-10 block min-[1024px]:hidden">
-      <img src="https://res.cloudinary.com/dgquchqc2/image/upload/v1750361713/menu_dahvfo.png" alt="menu" className="size-6"/>
-      </button>
-      <img src="https://res.cloudinary.com/dgquchqc2/image/upload/v1750359425/favicon_spilef.svg" alt="icon" className="size-5 object-contain min-[375px]:hidden" />
-      <img src="https://res.cloudinary.com/dgquchqc2/image/upload/v1750359181/logo_fbv0e7.svg" alt="logo" className="size-full object-cover hidden min-[375px]:block" />
-     </div>
-     {/* right content */}
-     <div className="h-10 w-20">
-      <Button variant={"outline"} className="size-full">Sign in</Button>
-     </div>
-     </div>
+      <div className="h-full flex justify-between items-center">
+        {/* left content */}
+        <div className="h-10 w-44 flex justify-start items-center">
+          {/* Dropdown button*/}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant={"ghost"}
+                size={"icon"}
+                className="min-[1024px]:hidden flex items-center justify-start"
+              >
+                <Menu className="size-7" />
+              </Button>
+            </DropdownMenuTrigger>
+            {/* Dropdown content */}
+            <DropdownMenuContent className="ml-3">
+              <DropdownMenuItem onClick={handleFeed}>Feeds</DropdownMenuItem>
+              <DropdownMenuItem onClick={handleAbout}>About</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          {theme === "dark" ? (
+            <>
+              <img
+                src="https://res.cloudinary.com/dgquchqc2/image/upload/v1750357672/favicon_migqwq.svg"
+                alt="icon-dark"
+                className="size-5 object-contain min-[375px]:hidden"
+              />
+              <img
+                src="https://res.cloudinary.com/dgquchqc2/image/upload/v1750397416/logo-dark_jweijj.svg"
+                alt="logo-dark"
+                className="h-full w-32 object-cover hidden min-[375px]:block"
+              />
+            </>
+          ) : (
+            <>
+              <img
+                src="https://res.cloudinary.com/dgquchqc2/image/upload/v1750359425/favicon_spilef.svg"
+                alt="icon"
+                className="size-5 object-contain min-[375px]:hidden"
+              />
+              <img
+                src="https://res.cloudinary.com/dgquchqc2/image/upload/v1750397284/logo_sk9cds.svg"
+                alt="logo"
+                className="h-full w-32 object-cover hidden min-[375px]:block"
+              />
+            </>
+          )}
+        </div>
+        {/* right content */}
+        <div className="h-10 lg:w-72 flex">
+          <div className="h-full w-40 hidden lg:flex lg:justify-center">
+           <Button variant={"ghost"} onClick={handleFeed}>
+             Feeds
+           </Button>
+           <Button variant={"ghost"} onClick={handleAbout}>
+             About
+           </Button>
+          </div>
+          {/* theme */}
+          <div className="h-full w-10">
+            <ModeToggle />
+          </div>
+          {/* sign in */}
+          <Button
+            variant={"outline"}
+            className="h-full w-20"
+            onClick={handleSignIn}
+          >
+            Sign in
+          </Button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
